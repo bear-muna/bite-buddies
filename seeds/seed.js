cb735a1d847784258b653ddbdbc63121e76ee379
Amman Nega <ammannega@gmail.com>
2023-05-05 15:40:11 -0700

    created tables using json data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,2 +1,23 @@
 const sequelize = require('../config/connection');
-const {}
+const {User, Application, Status} = require('../models');
+
+const userData = require('./userData.json');
+const applicationData = require('./applicationData.json');
+const statusData = require('./statusData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({force: true});
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    const applications = await Application.bulkCreate(applicationData);
+
+    const status = await Status.bulkCreate(statusData);
+
+    process.exit(0);
+};
+
+seedDatabase();
