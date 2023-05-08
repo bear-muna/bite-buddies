9a7ad3f3e9b36b416ca85d45f884163e723e76b8
bear-muna <bearmuna@gmail.com>
2023-05-08 14:00:33 -0700

    update user model with names

--- models/User.js
+++ models/User.js
@@ -19,6 +19,15 @@ User.init(
         username: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
         },
         password: {
             type: DataTypes.STRING,
