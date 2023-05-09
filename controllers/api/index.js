ebac1c01162d35e995e0edbecfe4617da548fbfb
Amman Nega <ammannega@gmail.com>
2023-05-09 11:56:34 -0700

    added HOST env variable to env file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userController');
-// const profileRoutes = require('./profileController');
+const profileRoutes = require('./profileController');
 // const cuisineRoutes = require('./cuisineController');
 // const messageRoutes = require('./messageController');
 
 router.use('/users', userRoutes);
-// router.use('/profiles', profileRoutes);
+router.use('/profiles', profileRoutes);
 // router.use('/cuisines', cuisineRoutes);
 // router.use('/messages', messageRoutes);
 
--- controllers/api/profileController.js
+++ controllers/api/profileController.js
@@ -4,6 +4,15 @@ const { Profile } = require('../../models');
 // create profile route
 router.post('/', async (req, res) => {
     try {
+        const profileTestData = await Profile.findOne({ where: { user_id: req.body.user_id } });
+
+        if(profileTestData) {
+            res
+            .status(400)
+            .json({ message: 'User already has a profile' });
+            return;
+        }
+
         const profileData = await Profile.create(req.body);
 
         
@@ -30,7 +39,7 @@ router.put('/', async (req, res) => {
             }
         );
 
-        res.status(500).json(profileData);
+        res.status(200).json(profileData);
     } catch (err) {
         res.status(500).json(err);
     }
