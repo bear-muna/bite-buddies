48995e523f1aa44a820ec671034b513e82bb9ddd
bear-muna <bearmuna@gmail.com>
2023-05-09 12:34:59 -0700

    created /messages/:sendID/:recID route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -79,14 +79,18 @@ router.get('/messages/:sendID/:recID', async (req, res) => {
             include: [User],
         });
 
-        const dbRecipientData = await Message.findAll();
+        const dbRecipientData = await Message.findAll({
+            where: {
+                sender_id: req.params.recID,
+                recipient_id: req.params.sendID,
+            },
+            include: [User],
+        });
 
-        res.json({dbSenderData, dbRecipientData});
+        const sendMessage = dbSenderData.map((mes) => mes.get({ plain: true }));
+        const recMessage = dbRecipientData.map((mes) => mes.get({ plain: true }));
 
-        // const sendMessage = dbSenderData.map((mes) => mes.get({ plain: true }));
-        // const recMessage = dbRecipientData.map((mes) => mes.get({ plain: true }));
-
-        // res.render('/message', { sendMessage, recMessage, logged_in: req.session.logged_in });
+        res.render('/message', { sendMessage, recMessage, logged_in: req.session.logged_in });
 
     } catch (error) {
         console.log(error);
