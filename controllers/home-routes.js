268acc09f2e4e8d39e15cbf8553216d08d51fcee
bear-muna <bearmuna@gmail.com>
2023-05-09 12:29:07 -0700

    working on message routes and added seeded more messages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -66,6 +66,32 @@ router.get('/profiles/:id', async (req, res) => {
         console.log(error);
         res.status(500).json({ msg: "Error loading profile", error });
     }
+});
+
+// GET messages between 2 Users
+router.get('/messages/:sendID/:recID', async (req, res) => {
+    try {
+        const dbSenderData = await Message.findAll({
+            where: {
+                sender_id: req.params.sendID,
+                recipient_id: req.params.recID,
+            },
+            include: [User],
+        });
+
+        const dbRecipientData = await Message.findAll();
+
+        res.json({dbSenderData, dbRecipientData});
+
+        // const sendMessage = dbSenderData.map((mes) => mes.get({ plain: true }));
+        // const recMessage = dbRecipientData.map((mes) => mes.get({ plain: true }));
+
+        // res.render('/message', { sendMessage, recMessage, logged_in: req.session.logged_in });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "Error loading profile", error });
+    }
 })
 
 module.exports = router;
