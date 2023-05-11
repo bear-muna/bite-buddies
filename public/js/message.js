4e5c0824d73304c55e6dc3d7a8e7958ce79c9d71
bear-muna <bearmuna@gmail.com>
2023-05-10 23:37:20 -0700

    message.js created

---
+++ public/js/message.js
@@ -0,0 +1,25 @@
+// TODO: Query selectors are not made yet
+
+const messageSend = async (e) => {
+    e.preventDefault();
+
+    const message = document.querySelector('#message').ariaValueMax.trim();
+    
+    if (message) {
+        const response = await fetch('/api/message', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ message }),
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert('Cannot send message');
+        }
+    }
+};
+
+document
+    .querySelector('#message-form')
+    .addEventListener('submit', messageSend);
